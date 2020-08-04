6f35a1e1db37f6973afa8c9704512ac869274b2b
daphne <kdh01215@naver.com>
2020-08-04 16:09:30 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = reuire('moongoose');
+
+
+const userSchema = mongoose.Schema({
+    name: {
+        tpye: String,
+        malength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
